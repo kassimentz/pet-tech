7e3e3d3f5af878fece068c829dbb44fdbe5b8ef4
kassimentz <kassimentz@gmail.com>
2023-06-24 11:56:48 -0300

    adicionando dados iniciais via script

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_produto (id, nome, descricao, preco, url_imagem) VALUES ('b8804555-5a3e-4cd2-9818-2271e795d594', 'Bolinha para gatos', 'Divertida', 50.99, 'URL 1');
+INSERT INTO tb_produto (id, nome, descricao, preco, url_imagem) VALUES ('7262bb6a-e5e3-46e2-946c-d37fa42a4140', 'Bolinha para caes', 'Divertida', 50.99, 'URL 2');
+INSERT INTO tb_produto (id, nome, descricao, preco, url_imagem) VALUES ('43edc4ff-42ac-482b-9a05-939a9c53e6fe', 'Catnip', 'Doidao', 10.99, 'URL 3');
+INSERT INTO tb_produto (id, nome, descricao, preco, url_imagem) VALUES ('17d492e5-35d3-42a3-bb01-b189c4c0360a', 'Banheiro para gatos', 'Higiene', 30.99, 'URL 4');
+INSERT INTO tb_produto (id, nome, descricao, preco, url_imagem) VALUES ('da1faa05-cd7d-43ef-83c4-97c96636f2c9', 'Comedouro', 'Automatico', 150.99, 'URL 5');
