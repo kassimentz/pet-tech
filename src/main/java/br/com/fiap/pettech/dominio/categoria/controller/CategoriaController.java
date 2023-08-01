ffb9c7f88adbfd2b57e8f5cd882009f065733175
kassimentz <kassimentz@gmail.com>
2023-08-01 20:28:24 -0300

    api de categoria finalizada

--- src/main/java/br/com/fiap/pettech/dominio/categoria/controller/CategoriaController.java
+++ src/main/java/br/com/fiap/pettech/dominio/categoria/controller/CategoriaController.java
@@ -7,10 +7,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/categorias")
@@ -22,12 +22,38 @@ public class CategoriaController {
     @GetMapping
     public ResponseEntity<Page<CategoriaDTO>> findAll(
         @RequestParam(value = "pagina", defaultValue = "0") Integer pagina,
-        @RequestParam(value = "quantidade", defaultValue = "0") Integer quantidade,
+        @RequestParam(value = "quantidade", defaultValue = "10") Integer quantidade,
         @RequestParam(value = "direcao", defaultValue = "DESC") String direcao,
         @RequestParam(value = "ordenacao", defaultValue = "nome") String ordenacao
     ){
         PageRequest pageRequest = PageRequest.of(pagina, quantidade, Sort.Direction.valueOf(direcao), ordenacao);
         var list = service.findAll(pageRequest);
         return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoriaDTO> findById(@PathVariable Long id)
+    {
+        CategoriaDTO categoriaDTO = service.findById(id);
+        return ResponseEntity.ok(categoriaDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoriaDTO> insert(@RequestBody CategoriaDTO dto) {
+        var categoriaSaved = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoriaSaved.getId()).toUri();
+        return ResponseEntity.created(uri).body(categoriaSaved);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoriaDTO> update(@PathVariable Long id, @RequestBody CategoriaDTO dto) {
+        dto = service.update(id, dto);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
